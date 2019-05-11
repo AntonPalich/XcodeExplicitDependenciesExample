845afe46f10dce8226ba6f988ec26e75c4d5e59e
Anton Schukin <anton.schukin@corp.badoo.com>
2019-05-11 18:50:30 +0100

    Added CommonFeature framework

---
+++ Modules/CommonFeature/CommonFeature/CommonFeature.h
@@ -0,0 +1,19 @@
+//
+//  CommonFeature.h
+//  CommonFeature
+//
+//  Created by Anton Schukin on 11/05/2019.
+//  Copyright © 2019 Anton Schukin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CommonFeature.
+FOUNDATION_EXPORT double CommonFeatureVersionNumber;
+
+//! Project version string for CommonFeature.
+FOUNDATION_EXPORT const unsigned char CommonFeatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonFeature/PublicHeader.h>
+
+
