7710a4ab21a828ff8a9dd8a98abde6879488ee00
Anton Schukin <anton.schukin@corp.badoo.com>
2019-05-11 16:14:21 +0100

    Added MyNetwork framework

---
+++ Modules/MyNetwork/MyNetwork/MyNetwork.h
@@ -0,0 +1,19 @@
+//
+//  MyNetwork.h
+//  MyNetwork
+//
+//  Created by Anton Schukin on 11/05/2019.
+//  Copyright © 2019 Anton Schukin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyNetwork.
+FOUNDATION_EXPORT double MyNetworkVersionNumber;
+
+//! Project version string for MyNetwork.
+FOUNDATION_EXPORT const unsigned char MyNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyNetwork/PublicHeader.h>
+
+
