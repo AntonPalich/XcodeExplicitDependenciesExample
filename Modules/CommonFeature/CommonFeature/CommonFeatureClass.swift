9ccaef5a7473a79c48e69750b5eb9df06496e6dc
Anton Schukin <anton.schukin@corp.badoo.com>
2019-05-11 18:57:19 +0100

    Added Chat and MyNetwork dependencies to CommonFeature framework and it's test target

---
+++ Modules/CommonFeature/CommonFeature/CommonFeatureClass.swift
@@ -0,0 +1,16 @@
+//
+//  CommonFeatureClass.swift
+//  CommonFeature
+//
+//  Created by Anton Schukin on 11/05/2019.
+//  Copyright © 2019 Anton Schukin. All rights reserved.
+//
+
+import Chat
+import MyNetwork
+import Foundation
+
+public final class CommonFeatureClass {
+    private let chatClass = ChatClass()
+    public init() {}
+}
