ca6677ef0df305d63d6ca1881175666507fdcb27
Anton Schukin <anton.schukin@corp.badoo.com>
2019-05-11 19:11:13 +0100

    Added SnapKit dependency to AppTwo application

--- Apps/AppTwo/AppTwo/ViewController.swift
+++ Apps/AppTwo/AppTwo/ViewController.swift
@@ -6,15 +6,22 @@
 //  Copyright © 2019 Anton Schukin. All rights reserved.
 //
 
+import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let view = UIView()
+        view.backgroundColor = .red
+        self.view.addSubview(view)
+
+        view.snp.makeConstraints { (make) -> Void in
+            make.width.height.equalTo(50)
+            make.center.equalTo(self.view)
+        }
     }
-
-
 }
 
