200f49a9ec6261e3f6a446eb26b776c9c52cc747
Anton Schukin <anton.schukin@corp.badoo.com>
2019-05-11 13:00:59 +0100

    Added Chat framework

---
+++ Modules/Chat/Chat/Chat.h
@@ -0,0 +1,19 @@
+//
+//  Chat.h
+//  Chat
+//
+//  Created by Anton Schukin on 11/05/2019.
+//  Copyright © 2019 Anton Schukin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Chat.
+FOUNDATION_EXPORT double ChatVersionNumber;
+
+//! Project version string for Chat.
+FOUNDATION_EXPORT const unsigned char ChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chat/PublicHeader.h>
+
+
