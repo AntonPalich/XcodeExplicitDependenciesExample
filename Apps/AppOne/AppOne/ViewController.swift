9327a9757bebcc5681c6f5fa01c7c3dc13ff18a1
Anton Schukin <anton.schukin@corp.badoo.com>
2019-05-11 16:22:24 +0100

    Added AppOne application

---
+++ Apps/AppOne/AppOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppOne
+//
+//  Created by Anton Schukin on 11/05/2019.
+//  Copyright © 2019 Anton Schukin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
